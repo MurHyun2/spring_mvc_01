e39bc8f5a18d82e835ed798cf19ee13884b48ad3
MurHyun2 <eddy152264@gmail.com>
2025-03-09 16:01:26 +0900

    섹션7. 정적 리소스, 뷰 템플릿

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -35,6 +35,12 @@ public class RequestBodyJsonController {
         response.getWriter().write("ok");
     }
 
+    /**
+     * @RequestBody HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     * @ResponseBody - 모든 메서드에 @ResponseBody 적용
+     * - 메시지 바디 정보 직접 반환(view 조회X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     */
     @ResponseBody
     @PostMapping("request-body-json-v2")
     public String requestBodyJsonV2(@RequestBody String messageBody) throws IOException {
@@ -46,6 +52,11 @@ public class RequestBodyJsonController {
         return "ok";
     }
 
+
+    /**
+     * @RequestBody 생략 불가능(@ModelAttribute 가 적용되어 버림)
+     * HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter (content-type:application/json)
+     */
     @ResponseBody
     @PostMapping("request-body-json-v3")
     public String requestBodyJsonV3(@RequestBody HelloData helloData) {
@@ -65,6 +76,13 @@ public class RequestBodyJsonController {
         return "ok";
     }
 
+    /**
+     * @RequestBody 생략 불가능(@ModelAttribute 가 적용되어 버림)
+     * HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter (content-type:application/json)
+     * @ResponseBody 적용
+     * - 메시지 바디 정보 직접 반환(view 조회X)
+     * - HttpMessageConverter 사용 -> MappingJackson2HttpMessageConverter 적용(Accept:application/json)
+     */
     @ResponseBody
     @PostMapping("request-body-json-v5")
     public HelloData requestBodyJsonV5(@RequestBody HelloData helloData) {
